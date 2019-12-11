894ffb229248fb9037dea7e5d2cb0cb5e2b0d8d8
pauromeropau <pauromeropau@gmail.com>
2019-12-11 12:28:57 +0100

    components

--- client/src/components/HowItWorks/HowItWorks.js
+++ client/src/components/HowItWorks/HowItWorks.js
@@ -7,16 +7,16 @@ class HowItWorks extends Component {
       <React.Fragment>
         <section className="HowItWorks">
           <section>
-            <h1>Recogemos</h1>
-            <p>Vamos a buscar tus cosas donde quieras, las protegemos para el transporte y nos las llevamos.</p>
+            <h1 className="HowItWorks-h1">Recogemos</h1>
+            <p className="HowItWorks-p">Vamos a buscar tus cosas donde quieras, las protegemos para el transporte y nos las llevamos.</p>
           </section>
           <section>
-            <h1>Guardamos</h1>
-            <p>Cuidamos tus cosas. Nuestras instalaciones están vigiladas 24 horas al día. No tienes que preocuparte de nada.</p>
+            <h1 className="HowItWorks-h1">Guardamos</h1>
+            <p className="HowItWorks-p">Cuidamos tus cosas. Nuestras instalaciones están vigiladas 24 horas al día. No tienes que preocuparte de nada.</p>
           </section>
           <section>
-            <h1>Devolvemos</h1>
-            <p>Te entregamos lo que necesites dónde y cuándo quieras. Sólo tienes que decírnoslo.</p>
+            <h1 className="HowItWorks-h1">Devolvemos</h1>
+            <p className="HowItWorks-p">Te entregamos lo que necesites dónde y cuándo quieras. Sólo tienes que decírnoslo.</p>
           </section>
         </section>
       </React.Fragment>
